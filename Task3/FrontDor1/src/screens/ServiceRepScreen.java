e38c4c7ac0f04347970fdd8ff3c3efe7bd97b70c
DOR <dorstern3@gmail.com>
2026-05-30 14:21:02 +0300

    Task 3 front22

--- Task3/FrontDor1/src/screens/ServiceRepScreen.java
+++ Task3/FrontDor1/src/screens/ServiceRepScreen.java
@@ -1,7 +1,7 @@
 package screens;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.geometry.NodeOrientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -16,50 +16,98 @@ public class ServiceRepScreen extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("GoNature - נציג שירות");
+        primaryStage.setTitle("GoNature - Service Representative");
 
         VBox vbox = new VBox(10);
         vbox.setPadding(new Insets(20));
-        vbox.setAlignment(Pos.TOP_RIGHT);
-        vbox.setNodeOrientation(NodeOrientation.RIGHT_TO_LEFT);
+        vbox.setAlignment(Pos.TOP_LEFT);
 
-        Label title = new Label("רישום לקוח חדש (מנוי משפחתי / מדריך קבוצות)");
+        
+        Label title = new Label("Service Representative Panel");
         title.setStyle("-fx-font-weight: bold; -fx-font-size: 14px;");
 
-        GridPane grid = new GridPane();
-        grid.setVgap(10);
-        grid.setHgap(10);
+    
+        TabPane tabPane = new TabPane();
+        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
 
-        ComboBox<String> roleComboBox = new ComboBox<>();
-        roleComboBox.getItems().addAll("מנוי משפחתי", "מדריך קבוצות");
-        roleComboBox.setPromptText("בחר סוג רישום");
+        Tab familyTab = new Tab("Family Subscription");
+        Tab singleTab = new Tab("Single Subscription");
+        Tab groupTab = new Tab("Group Guide");
 
-        grid.add(new Label("סוג רישום:"), 0, 0);
-        grid.add(roleComboBox, 1, 0);
+        // Tab 1: Family Subscription (With Family Members field)
+        VBox familyVBox = new VBox(10);
+        familyVBox.setPadding(new Insets(10));
+        GridPane familyGrid = new GridPane();
+        familyGrid.setVgap(10);
+        familyGrid.setHgap(10);
 
-        grid.add(new Label("שם פרטי:"), 0, 1);
-        grid.add(new TextField(), 1, 1);
+        familyGrid.add(new Label("First Name:"), 0, 0);
+        familyGrid.add(new TextField(), 1, 0);
+        familyGrid.add(new Label("Last Name:"), 0, 1);
+        familyGrid.add(new TextField(), 1, 1);
+        familyGrid.add(new Label("ID Number:"), 0, 2);
+        familyGrid.add(new TextField(), 1, 2);
+        familyGrid.add(new Label("Mobile Number:"), 0, 3);
+        familyGrid.add(new TextField(), 1, 3);
+        familyGrid.add(new Label("Email:"), 0, 4);
+        familyGrid.add(new TextField(), 1, 4);
+        familyGrid.add(new Label("Family Members Amount:"), 0, 5);
+        familyGrid.add(new TextField(), 1, 5);
 
-        grid.add(new Label("שם משפחה:"), 0, 2);
-        grid.add(new TextField(), 1, 2);
+        Button familySubmitBtn = new Button("Register to System");
+        familyVBox.getChildren().addAll(familyGrid, familySubmitBtn);
+        familyTab.setContent(familyVBox);
 
-        grid.add(new Label("מספר זהות:"), 0, 3);
-        grid.add(new TextField(), 1, 3);
+        // Tab 2: Single Subscription 
+        VBox singleVBox = new VBox(10);
+        singleVBox.setPadding(new Insets(10));
+        GridPane singleGrid = new GridPane();
+        singleGrid.setVgap(10);
+        singleGrid.setHgap(10);
 
-        grid.add(new Label("מס' טלפון נייד:"), 0, 4);
-        grid.add(new TextField(), 1, 4);
+        singleGrid.add(new Label("First Name:"), 0, 0);
+        singleGrid.add(new TextField(), 1, 0);
+        singleGrid.add(new Label("Last Name:"), 0, 1);
+        singleGrid.add(new TextField(), 1, 1);
+        singleGrid.add(new Label("ID Number:"), 0, 2);
+        singleGrid.add(new TextField(), 1, 2);
+        singleGrid.add(new Label("Mobile Number:"), 0, 3);
+        singleGrid.add(new TextField(), 1, 3);
+        singleGrid.add(new Label("Email:"), 0, 4);
+        singleGrid.add(new TextField(), 1, 4);
 
-        grid.add(new Label("אימייל:"), 0, 5);
-        grid.add(new TextField(), 1, 5);
+        Button singleSubmitBtn = new Button("Register to System");
+        singleVBox.getChildren().addAll(singleGrid, singleSubmitBtn);
+        singleTab.setContent(singleVBox);
 
-        grid.add(new Label("מספר בני משפחה (למנוי):"), 0, 6);
-        grid.add(new TextField(), 1, 6);
+        // Tab 3: Group Guide 
+        VBox groupVBox = new VBox(10);
+        groupVBox.setPadding(new Insets(10));
+        GridPane groupGrid = new GridPane();
+        groupGrid.setVgap(10);
+        groupGrid.setHgap(10);
 
-        Button submitBtn = new Button("הירשם למערכת");
-        
-        vbox.getChildren().addAll(title, grid, submitBtn);
+        groupGrid.add(new Label("First Name:"), 0, 0);
+        groupGrid.add(new TextField(), 1, 0);
+        groupGrid.add(new Label("Last Name:"), 0, 1);
+        groupGrid.add(new TextField(), 1, 1);
+        groupGrid.add(new Label("ID Number:"), 0, 2);
+        groupGrid.add(new TextField(), 1, 2);
+        groupGrid.add(new Label("Mobile Number:"), 0, 3);
+        groupGrid.add(new TextField(), 1, 3);
+        groupGrid.add(new Label("Email:"), 0, 4);
+        groupGrid.add(new TextField(), 1, 4);
 
-        Scene scene = new Scene(vbox, 400, 400);
+        Button groupSubmitBtn = new Button("Register to System");
+        groupVBox.getChildren().addAll(groupGrid, groupSubmitBtn);
+        groupTab.setContent(groupVBox);
+
+        // Add all tabs to TabPane
+        tabPane.getTabs().addAll(familyTab, singleTab, groupTab);
+
+        vbox.getChildren().addAll(title, tabPane);
+
+        Scene scene = new Scene(vbox, 500, 450);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
