48e3553c5e5ee6f5985213325d2860becee4be0e
Anna Papikyan <anna203200@gmail.com>
2026-06-05 20:03:57 +0300

    Pulled Lior's SQL files from main

---
+++ Third_Task/Db/LiorDB/sql commands/Select.sql
@@ -0,0 +1,6 @@
+SELECT * FROM gonature_db_new.parks;
+SELECT * FROM gonature_db_new.subscriber;
+SELECT * FROM gonature_db_new.guide;
+SELECT * FROM gonature_db_new.workers;
+SELECT * FROM gonature_db_new.`order`;
+
