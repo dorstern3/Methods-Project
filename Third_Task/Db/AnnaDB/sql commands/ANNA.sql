4507923fe736cd22cea1abb5e84375c6eef5b896
LiorGinat <liorginat9@gmail.com>
2026-06-25 19:44:42 +0300

    Welcome to GO-NATURE

--- Third_Task/Db/AnnaDB/sql commands/ANNA.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO gonature_db_new.order (park_name, order_date, entry_time, number_of_visitors, email, phone_number, type_of_visitor, status) 
-VALUES ('Banias', '2026-10-10', '10:00:00', 2, 'cancel_me@gmail.com', '050-0000000', 'Regular', 'Confirmed');
-INSERT INTO gonature_db_new.order (park_name, order_date, entry_time, number_of_visitors, email, phone_number, type_of_visitor, status) 
-VALUES ('Banias', '2026-10-10', '10:00:00', 2, 'waitlist@gmail.com', '050-1111111', 'Regular', 'On waiting list');
